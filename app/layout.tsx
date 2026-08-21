1551036987adb9334e32c926ea9cd403d1fe76d5
crowddevelopers08-arch <crowddevelopers08@gmail.com>
2026-08-21 15:13:32 +0530

    create a new lp for homeopathy code atteched.hks

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import BookingProvider from "@/component/BookingProvider";
+import Script from "next/script";
 
 const LOGO_URL = "https://res.cloudinary.com/x6ec5hqm/image/upload/v1786612784/alpha-logos.png";
 
@@ -88,7 +89,32 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Tag Manager */}
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-TRRXPCLF');
+            `,
+          }}
+        />
+      </head>
       <body className="min-h-full flex flex-col font-sans">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TRRXPCLF"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <BookingProvider>{children}</BookingProvider>
       </body>
     </html>
